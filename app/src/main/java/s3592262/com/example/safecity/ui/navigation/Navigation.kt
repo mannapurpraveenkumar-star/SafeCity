13a08ef51e938c79a41a5303f6f00f783857cfb8
mannapurpraveenkumar-star <mannapurpraveenkumar@gmail.com>
2026-03-23 11:29:10 +0000

    Change start destination and update composable calls

--- app/src/main/java/s3592262/com/example/safecity/ui/navigation/Navigation.kt
+++ app/src/main/java/s3592262/com/example/safecity/ui/navigation/Navigation.kt
@@ -1,20 +1,30 @@
 package s3592262.com.example.safecity.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.*
 import s3592262.com.example.safecity.ui.screens.*
 
 @Composable
 fun AppNavigation() {
+
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "splash") {
+    NavHost(navController = navController, startDestination = "report") {
 
-        composable("splash") { SplashScreen(navController = navController) }
-        composable("report") { ReportScreen(navController = navController) }
-        composable("map") { MapScreen(navController = navController) }
-        composable("stats") { StatsScreen(navController = navController) }
+        composable("splash") {
+            SplashScreen(navController)
+        }
+
+        composable("report") {
+            ReportScreen(navController)
+        }
+
+        composable("map") {
+            MapScreen(navController)
+        }
+
+        composable("stats") {
+            StatsScreen(navController)
+        }
     }
 }
